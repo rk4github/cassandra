eff1c26c8e707aec26aab2837694040ee02b6143
Sandip Singh <k.sandip96@gmail.com>
2015-07-27 08:58:25 +0530

    Renaming Variables

--- wrapper_remote.py
+++ wrapper_remote.py
@@ -4,20 +4,20 @@ import sys
 import os
 import ConfigParser
 
-
 action=sys.argv[1]
+keyspaceName=sys.argv[2]
 
 config = ConfigParser.ConfigParser()
 config.readfp(open(r'server.properties'))
-list_of_ips=config.get('servers', 'ip').split(",")
+list_of_ipsAddress=config.get('servers', 'ip').split(",")
 get_action=config.get('servers', action)
 
-print get_action
-new="123"
-new1="demo"
-for ip in list_of_ips:
-	command="python "+get_action+" "+new1+" "+new
-	ssh_cmd="ssh vagrant@"+ip+" "+'"%s"'%command
+#restorePoint="123"
+keyspaceName="demo"
+for ipAddress in list_of_ipsAddress:
+	#command="python "+get_action+" "+keyspaceName+" "+restorePoint
+	command="python "+get_action+" "+keyspaceName
+	ssh_cmd="ssh "+ipAddress+" "+'"%s"'%command
 	print ssh_cmd
 	os.system(ssh_cmd)
 
