cc424f2456154f553e4a7cc6f1795bcc326a802c
Sandip Singh <k.sandip96@gmail.com>
2015-07-31 19:08:06 +0530

    Add checks for delete operations

--- wrapper_remote_restore_script.py
+++ wrapper_remote_restore_script.py
@@ -9,13 +9,40 @@ keyspaceName=sys.argv[2]
 restorePoint=sys.argv[3]
 config = ConfigParser.ConfigParser()
 config.readfp(open(r'server.properties'))
-list_of_ipsAddress=config.get('servers', 'ip').split(",")
-get_action=config.get('servers', action)
+listOfIPAddress = config.get('servers', 'ip').split(",")
+getAction=config.get('servers', action)
 
-for ipAddress in list_of_ipsAddress:
-	command="python "+get_action+" "+keyspaceName+" "+restorePoint+ " </dev/null >restore.log 2>&1 &"
+print "List of Nodes"
+print listOfIPAddress 
+
+inputListOfIPAddress = input('Please provide nodes list comma separated values of node: i.e. "10.24.1.20,10.24.1.21"\n')
+
+print "Stopping Cassandra..."
+userInput = raw_input("Do you want to continue [Y/n]?")
+if userInput == "Y":
+	print("Cassandra Stopped")
+else:
+        sys.exit(1)
+print "Deleting commitlog"
+userInput = raw_input("Do you want to continue [Y/n]?")
+if userInput == "Y":
+        print("Deleted commitlog")
+else:
+        sys.exit(1)
+print "Deleting Key Space SSTtable"
+userInput = raw_input("Do you want to continue [Y/n]?")
+if userInput == "Y":
+        print("Deleted Key Space SST table")
+else:
+        sys.exit(1)
+
+
+for ipAddress in inputListOfIPAddress.split(','):
+	command="source /etc/profile;python "+getAction+" "+keyspaceName+" "+restorePoint+" "+userInput+" </dev/null >restore.log 2>&1 &"
 	ssh_cmd="ssh "+ipAddress+" "+'"%s"'%command
+	scpCommand = "scp restore_script.py " + ipAddress + ":/opt/" 
+	print scpCommand
 	print ssh_cmd
+	os.system(scpCommand)
 	os.system(ssh_cmd)
-
 
