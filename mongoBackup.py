9fa42aeb05691167815b9ad9129ca1a0209345b7
sandip <k.sandip96@gmail.com>
2015-09-17 10:42:40 +0530

    Add Mongodb Backup Script

--- mongoBackup.py
+++ mongoBackup.py
@@ -13,8 +13,7 @@ def exportMongoCollections():
     databaseName = dbName
     newListOfOutputFileNames = []
     for collectionName in getCollectionNames():
-        backupTime = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
-        outputFileName = collectionName + backupTime + ".json"
+        outputFileName = collectionName + ".json"
         newListOfOutputFileNames.append(outputFileName)
         commandToExportMongoCollection = "mongoexport --db "+databaseName+ " --collection "+collectionName+ " --out "+outputFileName
         print "Exporting Mongodb Collection " + collectionName + " from Database "+databaseName
@@ -22,25 +21,34 @@ def exportMongoCollections():
     return newListOfOutputFileNames
 
 def getNodeBackupS3Path():
-    bucketName = sys.argv[2]
     nodeName = (socket.gethostname())
     nodeS3Path = "s3://"+bucketName+"/"+nodeName
     return nodeS3Path
 
 def uploadMongoCollectionsToS3():
-    outputFileName = exportMongoCollections()
+    outputFileNames = exportMongoCollections()
     s3SyncDir = getNodeBackupS3Path()
-    for collectionName in outputFileName:
-        s3SyncCommand = "aws s3 cp "+ collectionName + " " + s3SyncDir + "/mongodb/" +dbName+"/"+ collectionName
-        print "Uploading Mongodb Collections : <Local-2-S3>"
+    for collectionName in outputFileNames:
+        backupTime = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
+        s3SyncCommand = "aws s3 cp "+ collectionName + " " + s3SyncDir + "/mongoBackup/" + backupTime+"/" +dbName+"/"+ collectionName
+        print "Uploading Mongodb Collection : <Local-2-S3>"
         print s3SyncCommand
         os.system(s3SyncCommand)
 
+def validateDatabase():
+    client = MongoClient()
+    databases = client.database_names()
+    if any(dbName in s for s in databases):
+        uploadMongoCollectionsToS3()
+    else:
+        print "Please Provide a Valid Database Name"
 
 def main():
-    uploadMongoCollectionsToS3()
+    validateDatabase()
 
 dbName = sys.argv[1]
+bucketName = sys.argv[2]
+
 main()
 
 
